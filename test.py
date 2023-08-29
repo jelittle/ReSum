37735977c8e18a63c5a8129bdcdb3a03e378d8e1
jelittle <joshlittle077@gmail.com>
2023-08-29 12:32:38 -0400

    added a bunchastuff

---
+++ test.py
@@ -0,0 +1,74 @@
+import spacy
+
+from spacytextblob.spacytextblob import SpacyTextBlob
+
+#push every line of toyota_camry.txt into a string list
+def getListCamry():
+    dict = []
+    with open('toyota_camry.txt', 'r') as f:
+        for line in f:
+            line=line.strip()
+            line=line.lower()
+            dict.append(line)
+    return dict
+def buildNlp():
+        nlp = spacy.load('en_core_web_sm')
+        nlp.add_pipe('spacytextblob')
+        return nlp
+
+class Summary:
+    def __init__(self, sent,nlp):
+        self.nlp = nlp
+        self.doc = list(self.nlp.pipe(sent))
+        self.nouns= self.__findNouns()
+        self.top5= self.__setTop5()
+        self.sentiment=self.__getAvgSentiment(self.top5)
+            
+    def __findNouns(self):
+        noun = {}
+        for x in self.doc:
+            for token in x:
+                if token.pos_ == "NOUN":
+                    #if noun non in noun dictionary, add it
+                    if token.text not in noun:
+                        noun[token.text] = 1
+                    else:
+                        noun[token.text] += 1 
+        return noun
+    def __setTop5(self):
+        top5 = sorted(self.nouns, key=self.nouns.get, reverse=True)[:5]
+        top5dict = {}
+        for x in top5:
+            top5dict[x] = []
+            for y in self.doc:
+                for token in y:
+                    if token.text == x:
+                        top5dict[x].append(y)
+        return top5dict   
+    def __getAvgSentiment(self,top5):
+        avg=0
+        sentiment=[]
+        for x in top5:
+            for y in top5[x]:
+                avg+=y._.blob.polarity
+            avg=avg/len(top5[x])
+            sentiment.append(avg)
+            avg=0
+        return sentiment
+                
+                
+               
+    
+lst = getListCamry()
+sent=lst
+
+nlp = buildNlp()
+summary = Summary(sent,nlp)
+for x in summary.top5:
+    print(x)
+
+
+print(summary.sentiment)
+
+print(summary.top5["mpg"])
+
---
+++ testDocker.py
@@ -0,0 +1,17 @@
+import time
+
+
+
+# Get user input and assign it to input_file
+#input_file = input("Enter the input file name: ")
+input_file="input"
+
+# Process input data
+# For example, reverse the string
+output_data = input_file[::-1]
+
+# Wait for 10 seconds
+time.sleep(10)
+
+# Write output data to output file
+print
